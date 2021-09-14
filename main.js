8c466dddebb2666460f2abe2a8e55fcd575a650d
mohamed <ragabmohame6@gmail.com>
2021-09-14 14:18:53 +0200

    Init

---
+++ main.js
@@ -0,0 +1,40 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+
+        varients: [
+            {
+                varientId: 2234,
+                varientColor: "green",
+                varientImage: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
+            },
+            {
+                varientId: 2235,
+                varientColor: "blue",
+                varientImage: "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg"
+            }
+        ],
+
+        cart: 0
+    },
+
+    methods: {
+        addToCart() {
+            this.cart++;
+        },
+
+        updateProduct(image) {
+            this.image = image;
+        },
+
+        removeFromCart() {
+            if(this.cart !== 0) {
+                this.cart--;
+            }
+        }
+    }
+})
